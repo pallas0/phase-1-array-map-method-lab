14bc0a0162313e1137e802d9a0cc9e582526989d
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-13 10:24:19 -0800

    commit tree 924f462644d91f4eaae0ce07a2757a95486e12b8

--- index.js
+++ index.js
@@ -11,6 +11,51 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+/*
+function titleCasedDemo(tutorials) {
+  return tutorials.map(function (phrase) {
+    return (phrase.split(" ").map(function (word) {
+      return word[0].toUpperCase() + word.slice(1, word.length);
+    })).join(" ");
+  })
 }
+*/
+/*
+const titleCasedDemo2 = tutorials.map((phrase) => {
+  return (phrase.split(" ").map(function (word) {
+    return word[0].toUpperCase() + word.slice(1, word.length);
+  })).join(" ");
+})
+*/
+
+const titleCased = (array=tutorials) => {
+  return array.map(function (phrase) {
+    return (phrase.split(" ").map(function (word) {
+      return word[0].toUpperCase() + word.slice(1, word.length);
+    })).join(" ");
+  })
+}
+
+/*
+const practice = [1, 2, 3]
+const practiceMap = practice.map((anything) =>{
+  return anything*anything;
+})
+
+console.log(practiceMap)
+/*
+
+/*
+function titleCasedDemo(array, cb) {
+  return array.map(phraseShift);
+}
+function phraseShift(phrase) {
+  //const changedPhrase =
+  return (phrase.split(" ").map(function (word) {
+    return word[0].toUpperCase() + word.slice(1, word.length);
+  })).join(" ");
+  //return changedPhrase.join(" ");
+}
+
+console.log(titleCased(tutorials));
+*/
